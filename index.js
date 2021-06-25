ad9a27fe932f99f11754de5ca686e98d9c93c176
Kirk Thomas <dablumaroon@gmail.com>
2021-06-25 12:45:05 -0400

    Having problem with inquirer; getting help from AskBCS.

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-const questions = require('./questionsForReadme')
+const rdme = require('./questionsForReadme.js')
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { 
-    inquirer.prompt(questions   )
+const promptUser = () => {
+    inquirer.prompt(rdme.questions).then((answers) => {
+        console.log(JSON.stringify(answers, null, '  '));
+    });
 }
 
+const generateREADME = (answers) => {
+    
+}
 // TODO: Create a function to initialize app
-function init() { }
+const init = () => {
+    promptUser()
+        .then((answers) => writeFileAsync('index.html', generateREADME(answers)))
+        .then(() => console.log('Successfully wrote to index.html'))
+        .catch((err) => console.error(err));
+};
 
 // Function call to initialize app
 init();
